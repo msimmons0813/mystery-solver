fb1cc32b54f77ef638379951c4b1d283178be319
msimmons0813 <msimmons0813@gmail.com>
2018-08-09 15:47:56 -0400

    Initial Commit

---
+++ MysterySolver/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MysterySolver {
+    class Program {       
+
+        static void Main(string[] args) {
+            string filepath = "../../participants.txt";
+
+            var fileLines = File.ReadAllLines(filepath).Skip((1)).ToList();
+
+            List<string> personlst = new List<string>();
+            List<string> liarlst = new List<string>();
+
+            foreach (string line in fileLines) {
+                if (line.Contains(",")){
+                    string person = line.Substring(0, line.IndexOf(','));
+                    personlst.Add(person);
+                }
+                else {
+                    liarlst.Add(line);
+                }
+            }
+
+            var truthful = personlst.Except(liarlst);
+            var liars = personlst.Except(truthful);
+
+            foreach (string p in truthful) {
+                Console.WriteLine(p + " is truthful");
+            }
+
+            foreach (string p in liars) {
+                Console.WriteLine(p + " is a liar");
+            }
+
+
+        }
+    }
+}
